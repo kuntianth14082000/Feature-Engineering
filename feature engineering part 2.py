554745d3442780d2d2f979cb9a807f874a6b5eea
kuntianth14082000 <58073841+kuntianth14082000@users.noreply.github.com>
2021-01-17 12:29:28 +0530

    Add files via upload

--- feature engineering part 2.py
+++ feature engineering part 2.py
@@ -26,4 +26,22 @@ pd.get_dummies(df,drop_first=True).head()
 import pandas as pd
 df=pd.read_csv(r'D:\sabir\python\Datasets\benze.csv',usecols=['X0','X1','X2','X3','X4','X5','X6'])
 df.head()
-df['X0'].unique()
+for i in df.columns:
+    print(len(df[i].unique()))
+lst_10=df.X1.value_counts().sort_values(ascending=False).head(10).index
+lst_10=list(lst_10)
+
+import numpy as np
+for categories in lst_10:
+    df[categories]=np.where(df['X1']==categories,1,0)
+df[lst_10]
+lst_10.append('X1')
+
+'''
+#   mean Encoding
+'''
+df=pd.read_csv(r"D:\sabir\python\Datasets\titanic.csv",usecols=['Survived','Cabin'])
+mean_ordinal=df.groupby(['Cabin'])['Survived'].mean().to_dict()
+mean_ordinal
+df['mean_ordinal_encoding']=df['Cabin'].map(mean_ordinal)
+df.head()
